d4021a604d995f2e834e2033cadd4faa6b1dbd31
eveins <maxmost512@gmail.com>
2020-04-09 22:48:12 +0300

    bugfix: minor fixes

--- server/src/middleware/multer.js
+++ server/src/middleware/multer.js
@@ -1,28 +1,29 @@
-const path = require('path');
-const fs = require('fs');
-const multer = require('multer');
+const path = require( 'path' );
+const fs = require( 'fs' );
+const multer = require( 'multer' );
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+const storage = multer.diskStorage( {
+  destination: function ( req, file, cb ) {
 
-    const fileDestination = path.resolve(__dirname,
-      `./../../uploads/profilePicture`);
+    const fileDestination = path.resolve( __dirname,
+      '..', '..', 'uploads', 'profilePicture' );
 
-    if ( !fs.existsSync(fileDestination)) {
-      fs.mkdirSync(fileDestination);
+    if( !fs.existsSync( fileDestination ) ) {
+      fs.mkdirSync( fileDestination,
+        { recursive: true } );
     }
 
-    cb(null, fileDestination);
+    cb( null, fileDestination );
   },
-  filename: function (req, file, cb) {
+  filename: function ( req, file, cb ) {
 
-    const extension = path.extname(file.originalname);
-    const newFileName = `${ Date.now() }${ extension }`;
-    cb(null, newFileName);
+    const extension = path.extname( file.originalname );
+    const newFileName = `${Date.now()}${extension}`;
+    cb( null, newFileName );
   },
-});
+} );
 
-module.exports.saveProfilePicture = multer({
+module.exports.saveProfilePicture = multer( {
   storage,
-}).single('profilePicture');
+} ).single( 'profilePicture' );
 
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://root:pass@mongodb:27017/fc_chat', {
+mongoose.connect('mongodb://mongodb:27017/fc_chat', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 mongoose.connection.once( 'open', () => console.log( 'mongodb successfully connected' ) );
+mongoose.connection.on('error', err => console.log(err));
 
 module.exports.User = require('./User.js');
 module.exports.Chat = require('./Chat.js');
